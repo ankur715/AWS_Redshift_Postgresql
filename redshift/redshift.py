bfda4dcb91b5746b5ff7dfac8497e57794a917d2
Ankur Patel <p.ankur.715@gmail.com>
2019-04-25 01:54:39 -0400

    Update redshift.py

--- redshift/redshift.py
+++ redshift/redshift.py
@@ -1,6 +1,6 @@
 import psycopg2
 
-ztables = ["product", "category"]
+ztables = ["category", "region", "vendor"]
 # sql tables created and saved, load into redshift using copy
 def redshift():
     conn_string = psycopg2.connect(dbname='dev', host='redshift-cluster-1.cogamnpawfrb.us-east-2.redshift.amazonaws.com',
@@ -22,7 +22,7 @@ def redshift():
 redshift()
 
 
-
+# # individually:
 
 # conn_string = psycopg2.connect(dbname='dev', host='redshift-cluster-1.cogamnpawfrb.us-east-2.redshift.amazonaws.com', port='5439', user='ankur', password='Ankur715')
 # cur = conn_string.cursor();
@@ -36,23 +36,5 @@ redshift()
 # conn_string.commit()
 #
 # cur.execute("select * from category;")
-# results = cur.fetchall()
-# print(results)
-
-
-
-# conn_string = psycopg2.connect(dbname='dev', host='redshift-cluster-1.cogamnpawfrb.us-east-2.redshift.amazonaws.com', port='5439', user='ankur', password='Ankur715')
-# cur = conn_string.cursor();
-#
-# file = open("product.sql", 'r')
-# cur.execute(file.read())
-# conn_string.commit()
-#
-# sql = """copy product from 's3://ankur-zagiiii-bucket/product.csv' credentials 'aws_iam_role=arn:aws:iam::927492682481:role/myRedshiftRole' delimiter ',' region 'us-west-2';"""
-# cur.execute(sql)
-# conn_string.commit()
-#
-#
-# cur.execute("select * from product;")
 # results = cur.fetchall()
 # print(results)
